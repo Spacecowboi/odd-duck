dde9b02b987e1d6b71bdb4ffe14d0aea5718b725
Spacecowboi <cunningham2192@gmail.com>
2023-07-24 16:29:57 -0700

    added more js, functions to display on results

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function Product(name, imageSource) {
 
 // Product array
 
-const products = [];
+let products = [];
 
 // Create instantiated objects of the products and push them into the product array
 function createProduct(name, imageSource){
@@ -33,3 +33,48 @@ function getRandomProducts(){
   return uniqueProducts;
 }
 
+// Display the products in the browser
+
+function showProducts(){
+  let productBox = document.getElementById('productBox');
+  productBox.innerHTML = '';
+
+  let randomProducts = getRandomProducts();
+
+  randomProducts.forEach(function(product){
+    let imgElement = document.createElement('img');
+    imgElement.src = Product.imageSource;
+    imgElement.alt = Product.name;
+    imgElement.addEventListener('click', function(){
+      product.timesClicked++;
+      console.log(Product.name + ' clicked! Votes: ' + product.timesClicked);
+      showProducts;
+    });
+    productBox.appendChild(imgElement);
+  });
+  console.log('randomProducts:', randomProducts);
+}
+
+function displayResults(){
+  let resultsBox = document.getElementById('resultsBox');
+  resultsBox.innerHTML = '';
+
+  products.forEach(function(product){
+    let resultElement = documnet.createElement('p');
+    let votes = Product.timesClicked;
+    let timesShown = Product.timesShown;
+
+    let voteText = 'vote';
+    if(votes !== 1) {
+      voteText = 'votes';
+    }
+    let shownText = 'time';
+    if (timesShown !== 1) {
+      shownText = 'times';
+    }
+    resultElement.textContent = `${Product.name} had ${votes} ${voteText}, and was seen ${timesShown} ${shownText}. `;
+    resultsBox.appendChild(resultElement);
+  });
+}
+
+//
