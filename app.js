a85e3685a4352693d17f43d44079eeb9e707473c
Spacecowboi <cunningham2192@gmail.com>
2023-07-26 14:27:22 -0700

    added chart, local storage functionality

--- app.js
+++ app.js
@@ -17,14 +17,48 @@ function createProduct(name, imageSource){
   products.push(product);
 }
 
+function initializeProducts() {
+  const savedProducts = JSON.parse(localStorage.getItem('products'));
+
+  if (savedProducts) {
+    products = savedProducts;
+  } else {
+    createProduct('bag', 'img/bag.jpg');
+    createProduct('banana', 'img/banana.jpg');
+    createProduct('bathroom', 'img/bathroom.jpg');
+    createProduct('boots', 'img/boots.jpg');
+    createProduct('breakfast', 'img/breakfast.jpg');
+    createProduct('bubblegum', 'img/bubblegum.jpg');
+    createProduct('chair', 'img/chair.jpg');
+    createProduct('cthulhu', 'img/cthulhu.jpg');
+    createProduct('dog-duck', 'img/dog-duck.jpg');
+    createProduct('dragon', 'img/dragon.jpg');
+    createProduct('pen', 'img/pen.jpg');
+    createProduct('pet-sweep', 'img/pet-sweep.jpg');
+    createProduct('scissors', 'img/scissors.jpg');
+    createProduct('shark', 'img/shark.jpg');
+    createProduct('sweep', 'img/sweep.png');
+    createProduct('tauntaun', 'img/tauntaun.jpg');
+    createProduct('unicorn', 'img/unicorn.jpg');
+    createProduct('water-can', 'img/water-can.jpg');
+    createProduct('wine-glass', 'img/wine-glass.jpg');
+
+    //voting rounds
+  }
+}
+
+// Save products to local storage
+function saveProductsToLocalStorage() {
+  localStorage.setItem('products', JSON.stringify(products));
+}
+
 let currentRoundProducts = [];
-// let previousRoundProducts = [];
+
 
 // randomly get three unique products for display
 function getRandomProducts(){
   let uniqueProducts = [];
   let productCount = products.length;
-  // let allSeenProducts = currentRoundProducts.concat(previousRoundProducts);
 
   while(uniqueProducts.length < 3) {
     let randomIndex = Math.floor(Math.random() * productCount);
@@ -36,8 +70,6 @@ function getRandomProducts(){
     }
   }
   console.log('UNIQUE PRODUCTS', uniqueProducts, 'CURRENT ROUND PRODUCTS', currentRoundProducts);
-  //This clones the currentRoundProducts array INTO the previous rounds array before updating it. Please god work 
-  // currentRoundProducts = [];
 
   // populate an empty array with the seen unique products
   currentRoundProducts = uniqueProducts.slice();
@@ -90,29 +122,9 @@ function displayResults(){
 }
 
 //Products created from constructor which are then added to the products array
-createProduct('bag', 'img/bag.jpg');
-createProduct('banana', 'img/banana.jpg');
-createProduct('bathroom', 'img/bathroom.jpg');
-createProduct('boots', 'img/boots.jpg');
-createProduct('breakfast', 'img/breakfast.jpg');
-createProduct('bubblegum', 'img/bubblegum.jpg');
-createProduct('chair', 'img/chair.jpg');
-createProduct('cthulhu', 'img/cthulhu.jpg');
-createProduct('dog-duck', 'img/dog-duck.jpg');
-createProduct('dragon', 'img/dragon.jpg');
-createProduct('pen', 'img/pen.jpg');
-createProduct('pet-sweep', 'img/pet-sweep.jpg');
-createProduct('scissors', 'img/scissors.jpg');
-createProduct('shark', 'img/shark.jpg');
-createProduct('sweep', 'img/sweep.png');
-createProduct('tauntaun', 'img/tauntaun.jpg');
-createProduct('unicorn', 'img/unicorn.jpg');
-createProduct('water-can', 'img/water-can.jpg');
-createProduct('wine-glass', 'img/wine-glass.jpg');
 
-//voting rounds
 let rounds = 0;
-let maxRounds = 20; //testing purposes
+let maxRounds = 20;
 
 // Event Listener for results
 document.getElementById('viewResults').addEventListener('click',function() {
@@ -130,6 +142,8 @@ document.getElementById('viewResults').addEventListener('click',function() {
     });
   });
   console.log('View Clicks! Voting is over.');
+
+  saveProductsToLocalStorage();
 });
 
 //getting the product by name
@@ -137,16 +151,89 @@ function getProductByName(name){
   return products.find((product) => product.name === name);
 }
 
+// Create and populate a chart with our data collected from rounds of voting
+
+const ctx = document.getElementById('newChart').getContext('2d');
+let myChart;
+
+// Function to update the chart with the latest voting data
+function updateChart() {
+  const productNames = products.map((product) => product.name);
+  const timesShownData = products.map((product) => product.timesShown);
+  const timesClickedData = products.map((product) => product.timesClicked);
+
+  if (myChart) {
+    myChart.data.labels = productNames;
+    myChart.data.datasets[0].data = timesShownData;
+    myChart.data.datasets[1].data = timesClickedData;
+    myChart.update();
+  }
+}
+// Function to create chart
+function createChart() {
+  const productNames = products.map((product) => product.name);
+  const timesShownData = products.map((product) => product.timesShown);
+  const timesClickedData = products.map((product) => product.timesClicked);
+
+
+  myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: productNames,
+      datasets: [
+        {
+          label: 'Times Shown',
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderColor: 'rgba(75, 192, 192, 1)',
+          borderWidth: 1,
+          data: timesShownData,
+        },
+        {
+          label: 'Times Clicked',
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+          borderWidth: 1,
+          data: timesClickedData,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      scales: {
+        y: {
+          beginAtZero: true,
+          ticks: {
+            color: 'black', // Set the color of y-axis ticks text
+          },
+        },
+      },
+      plugins: {
+        legend: {
+          labels: {
+            color: 'black', // Set the color of legend text
+          },
+        },
+      },
+    },
+  });
+}
+
 //Start votes
+initializeProducts();
 showProducts();
+
+document.getElementById('viewResults').addEventListener('click', function () {
+  displayResults();
+  updateChart(); // Update the chart with the latest voting data
+});
 
 //Check if votes are completed
 function voteCheck(){
   rounds++;
   if(rounds <= maxRounds) {
 
-    //Updating the previous product array with the current round products
-    // previousRoundProducts = currentRoundProducts.slice();
+    createChart();
+    updateChart();
 
   } else {
     displayResults();
